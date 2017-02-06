af8b4f352dc41ea1949c389ce0bbd762728a3c90
ArthurJiang <arthursjiang@hotmail.com>
2017-02-06 11:52:04 +0800

    add markdown mode

---
+++ README.md
@@ -0,0 +1,5 @@
+## Installation
+Clone this repo to `~/.emacs.d`:
+```shell
+git clone https://github.com/ArthurJiang/.emacs.d.git ~/.emacs.d
+```
--- init.el
+++ init.el
@@ -1,14 +1,23 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-;; (package-initialize)
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("popkit" . "https://elpa.popkit.org/packages/"))
+(package-initialize)
 
+
+;; Org mode
 (require 'org)
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+
+;; Markdown mode
+(require 'markdown-mode)
 
 (provide 'init)
 
