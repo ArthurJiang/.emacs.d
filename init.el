fb0570cfe7fe52709084dc972a52d54cd37bd5d3
ArthurJiang <arthursjiang@hotmail.com>
2017-02-06 10:48:33 +0800

    init, enable for org extension

---
+++ init.el
@@ -0,0 +1,14 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
+
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+(provide 'init)
+
