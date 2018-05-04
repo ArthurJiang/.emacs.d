24d7c12f54e30b6ed806b859bb3aa85b7b863ff4
ArthurJiang <arthursjiang@hotmail.com>
2018-05-04 18:37:14 +0800

    add edit mode

---
+++ lisp/init-editing.el
@@ -0,0 +1,12 @@
+;;; init-editing.el --- editing configuration
+
+;;; Commentary:
+;;
+;; editing configuration
+;;
+
+;;; Code:
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C--") 'advertised-undo)
+(provide 'init-editing)
+;;; init-editing.el ends here
