906fe8d50cb2e7b5a10cc21d5aed704df54a4d1a
ArthurJiang <arthursjiang@hotmail.com>
2018-05-07 11:52:50 +0800

    enable pair mode, re kbd open link in org mode

--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -6,6 +6,7 @@
 ;;
 
 ;;; Code:
+(electric-pair-mode t)
 (global-set-key (kbd "RET") 'newline-and-indent)
 (global-set-key (kbd "C--") 'advertised-undo)
 (provide 'init-editing)
--- lisp/init-org.el
+++ lisp/init-org.el
@@ -13,6 +13,8 @@
 (add-hook 'org-mode-hook 'turn-on-org-cdlatex)
 (add-hook 'org-mode-hook
 	  (lambda () (local-set-key (kbd "C-l C-p") 'org-preview-latex-fragment)))
+(add-hook 'org-mode-hook
+	  (lambda () (local-set-key (kbd "C-o") 'org-open-at-point)))
 
 ;; Making emacs find latex in os X, GUI Emacx (so that C-l C-p(default is C-c C-x C-l) works on orgmode)
 (when (and *is-a-mac* *is-gui*)
