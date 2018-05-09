e9b3e87c07966c46f34db6030f3b6c4c6cdbf875
ArthurJiang <arthursjiang@hotmail.com>
2018-05-09 15:41:28 +0800

    change color for diff-hl

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -8,10 +8,15 @@
 ;;; Code:
 (require-package 'diff-hl)
 (when *is-gui*
-  (global-diff-hl-mode)
   ;; Enable it only in 'prog-mode buffers, with vc-dir integration:
   ;; (add-hook 'prog-mode-hook 'turn-on-diff-hl-mode)
   ;; (add-hook 'vc-dir-mode-hook 'turn-on-diff-hl-mode)
+  (custom-set-faces
+   '(diff-hl-change ((t (:backgroung "#ef921e"))))
+   '(diff-hl-insert ((t (:background "#82d2d7"))))
+   '(diff-hl-delete ((t (:background "#f59cb1"))))
+   )
+  (global-diff-hl-mode 1)
   )
 (provide 'init-git)
 ;;; init-git.el ends here
