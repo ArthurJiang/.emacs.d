3bc76da3986d0e7899bddfaa1a794b4edb11d024
ArthurJiang <arthur@ArthurJiangs-Mac-mini.local>
2018-05-10 00:14:56 +0800

    add session, smex

--- init.el
+++ init.el
@@ -82,7 +82,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (manoj-dark)))
- '(package-selected-packages (quote (flycheck))))
+ '(package-selected-packages (quote (session smex flycheck))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- lisp/init-editing.el
+++ lisp/init-editing.el
@@ -11,6 +11,7 @@
 (global-set-key (kbd "C--") 'advertised-undo)
 (global-hl-line-mode t)
 (global-set-key "\C-cs" 'eshell)
+
 ;; Column limitation
 (require 'whitespace)
 (setq whitespace-line-column 100)
@@ -21,6 +22,20 @@
 (when *is-gui*
   (custom-set-variables '(linum-format 'dynamic))
   (global-linum-mode 1))
+
+;; Smex
+(require-package 'smex)
+(smex-initialize) ; Can be omitted. This might cause a (minimal) delay  when Smex is auto-initialized on its first run.
+
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; This is your old M-x.
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+
+;; Session
+(desktop-save-mode 1)
+(require-package 'session)
+(add-hook 'after-init-hook 'session-initialize)
 
 (provide 'init-editing)
 ;;; init-editing.el ends here
