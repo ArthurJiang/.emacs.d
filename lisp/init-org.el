1c48e17bf7deb779e6c348e26530ade624699966
ArthurJiang <arthursjiang@hotmail.com>
2018-05-18 21:03:37 +0800

    add more kbd for org mode

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -17,12 +17,26 @@
 	  (lambda () (local-set-key (kbd "C-l C-p") 'org-preview-latex-fragment)))
 (add-hook 'org-mode-hook
 	  (lambda () (local-set-key (kbd "C-o") 'org-open-at-point)))
+(add-hook 'org-mode-hook
+	  (lambda () (local-set-key (kbd "C-c ls") 'org-store-link)))
+(add-hook 'org-mode-hook
+	  (lambda () (local-set-key (kbd "C-c li") 'org-insert-link)))
+(add-hook 'org-mode-hook
+	  (lambda () (local-set-key (kbd "C-c a") 'org-agenda)))
+(add-hook 'org-mode-hook
+	  (lambda () (local-set-key (kbd "C-c i") 'org-clock-in)))
+(add-hook 'org-mode-hook
+	  (lambda () (local-set-key (kbd "C-c o") 'org-clock-out)))
+(add-hook 'org-mode-hook
+	  (lambda () (local-set-key (kbd "C-c s") 'org-schedule)))
 
 (load-file "~/.emacs.d/lisp/macro.el")
 
 (add-hook 'org-mode-hook
 	  (lambda () (local-set-key (kbd "C-l C-f") 'latex-formula)))
-
+(add-hook 'org-mode-hook
+	  (lambda () (local-set-key (kbd "C-c q") 'insert-quote-in-org)))
+(setq org-log-done 'note)
 ;; Making emacs find latex in os X, GUI Emacx (so that C-l C-p(default is C-c C-x C-l) works on orgmode)
 (when (and *is-a-mac* *is-gui*)
   (setenv "PATH" (concat ":/Library/TeX/texbin/" (getenv "PATH")))
