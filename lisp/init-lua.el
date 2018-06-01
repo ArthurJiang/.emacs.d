1d7ce74384d5f025712dd26d268b4baca369aaa7
Arthur Jiang <sjian@nixml01.iwyqudjmwhluxl1cvppatjr3bc.lx.internal.cloudapp.net>
2018-06-01 17:20:12 +0800

    add new snippets for lua

--- lisp/init-lua.el
+++ lisp/init-lua.el
@@ -11,6 +11,7 @@
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
+(setq lua-indent-level 2)
 
 (provide 'init-lua)
 ;;; init-lua.el ends here
