2342cb3a34cd3111de959c9a514eb6838c2b37ff
ArthurJiang <arthursjiang@hotmail.com>
2018-07-03 18:38:31 +0800

    add markdown snippet

--- lisp/init-snippet.el
+++ lisp/init-snippet.el
@@ -9,6 +9,6 @@
 (add-to-list 'load-path "~/.emacs.d/plugins/yasnippet")
 (require 'yasnippet)
 (yas-global-mode 1)
-
+(advice-remove 'yas--auto-fill-wrapper #'ignore)
 (provide 'init-snippet)
 ;;; init-snippet.el ends here
